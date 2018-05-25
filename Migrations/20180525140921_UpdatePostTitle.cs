6203dbd16ba0ba86516a9cbce5c6963680ad2dc1
banerjia <banerjia@iu.edu>
2018-05-25 10:21:59 -0400

    First Commit

---
+++ Migrations/20180525140921_UpdatePostTitle.cs
@@ -0,0 +1,145 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace aspnetcoreapp.Migrations
+{
+    public partial class UpdatePostTitle : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Posts_Blogs_BlogId",
+                table: "Posts");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Posts",
+                table: "Posts");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Blogs",
+                table: "Blogs");
+
+            migrationBuilder.DropColumn(
+                name: "PostId",
+                table: "Posts");
+
+            migrationBuilder.DropColumn(
+                name: "BlogId",
+                table: "Blogs");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Posts",
+                maxLength: 512,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "BlogId",
+                table: "Posts",
+                nullable: false,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "Id",
+                table: "Posts",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "Id",
+                table: "Blogs",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Posts",
+                table: "Posts",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Blogs",
+                table: "Blogs",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Posts_Blogs_BlogId",
+                table: "Posts",
+                column: "BlogId",
+                principalTable: "Blogs",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Posts_Blogs_BlogId",
+                table: "Posts");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Posts",
+                table: "Posts");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Blogs",
+                table: "Blogs");
+
+            migrationBuilder.DropColumn(
+                name: "Id",
+                table: "Posts");
+
+            migrationBuilder.DropColumn(
+                name: "Id",
+                table: "Blogs");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Posts",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 512);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "BlogId",
+                table: "Posts",
+                nullable: false,
+                oldClrType: typeof(Guid));
+
+            migrationBuilder.AddColumn<int>(
+                name: "PostId",
+                table: "Posts",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn);
+
+            migrationBuilder.AddColumn<int>(
+                name: "BlogId",
+                table: "Blogs",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Posts",
+                table: "Posts",
+                column: "PostId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Blogs",
+                table: "Blogs",
+                column: "BlogId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Posts_Blogs_BlogId",
+                table: "Posts",
+                column: "BlogId",
+                principalTable: "Blogs",
+                principalColumn: "BlogId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
---
+++ Models/Blogging.cs
@@ -0,0 +1,37 @@
+﻿using Pomelo.EntityFrameworkCore.MySql;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System;
+
+namespace aspnetcoreapp.Models
+{
+    public class BloggingContext:DbContext
+    {
+        public BloggingContext(DbContextOptions<BloggingContext> options) : base(options){}
+
+        public DbSet<Blog> Blogs{get;set;}
+        public DbSet<Post> Posts{get;set;}
+    }
+
+    public class Blog
+    {
+        public Guid Id {get;set;}
+        public string Url {get;set;}
+        public List<Post> Posts {get;set;}
+    }
+
+    public class Post
+    {
+        public Guid Id {get;set;}
+
+        [Required]
+        [MaxLength(512)]
+        public string Title {get; set;}
+        public string Content {get;set;}
+
+        [ForeignKey("Blog")]
+        public Guid BlogId {get;set;}
+    }
+}
